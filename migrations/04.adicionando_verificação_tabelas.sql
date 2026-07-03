148460024ae4f75caa5dbb89e0d2731744d3966e
Amanda-Gabi <gabriellaamanda114@gmail.com>
2026-07-03 14:07:46 -0300

    Create 04.adicionando_verificação_tabelas.sql
    criamos verificações em CEP, CNPJ, CPF, TELEFONE e PLACA que são informções de algumas tabelas, fizemos isso para que informações inválidas não sejam adicionadas

---
+++ migrations/04.adicionando_verificação_tabelas.sql
@@ -0,0 +1,23 @@
+ALTER TABLE usuario 
+    ADD CONSTRAINT check_telefone_usuario 
+    CHECK (telefone IS NULL OR telefone ~ '^[0-9]+$');
+
+ALTER TABLE estabelecimento 
+    ADD CONSTRAINT check_cnpj_numeros 
+    CHECK (cnpj ~ '^[0-9]{14}$');
+
+ALTER TABLE entregador 
+    ADD CONSTRAINT check_cpf_numeros 
+    CHECK (cpf ~ '^[0-9]{11}$');
+
+ALTER TABLE entregador 
+    ADD CONSTRAINT check_telefone_entregador 
+    CHECK (telefone IS NULL OR telefone ~ '^[0-9]+$');
+
+ALTER TABLE entregador 
+    ADD CONSTRAINT check_placa_numeros_letras 
+    CHECK (placa IS NULL OR placa ~ '^[A-Z0-9]+$');
+
+ALTER TABLE endereco 
+    ADD CONSTRAINT check_cep_numeros 
+    CHECK (cep ~ '^[0-9]{8}$');
