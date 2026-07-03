d6abc41f28536723ca76e875bf8dd91c56cef27a
Amanda-Gabi <gabriellaamanda114@gmail.com>
2026-07-03 13:30:38 -0300

    Update 03_sistema_de_registros_de_pedidos.sql
    mudei de MySQL para PostgreSQL para rodar no supabase

--- migrations/03_sistema_de_registros_de_pedidos.sql
+++ migrations/03_sistema_de_registros_de_pedidos.sql
@@ -1,21 +1,19 @@
 CREATE TABLE pedido (
-    id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+    id_pedido SERIAL PRIMARY KEY,
     id_usuario INT NOT NULL,
     id_estabelecimento INT NOT NULL,
     id_endereco INT NOT NULL,
-    status ENUM('pendente','confirmado','preparando','saiu_entrega','entregue','cancelado') 
-        DEFAULT 'pendente',
-    valor_total DECIMAL(10,2) DEFAULT 0.00,
-    data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(25) CHECK (status IN ('aguardando_pagamento','pendente','confirmado','preparando','saiu_entrega','entregue','cancelado')) DEFAULT 'aguardando_pagamento',
+    valor_total DECIMAL(10,2) NOT NULL CHECK (valor_total >= 0.00),
+    data_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
     FOREIGN KEY (id_estabelecimento) REFERENCES estabelecimento(id_estabelecimento),
     FOREIGN KEY (id_endereco) REFERENCES endereco(id_endereco)
 );
 
-
 CREATE TABLE pedido_item (
-    id_pedido_item INT PRIMARY KEY AUTO_INCREMENT,
+    id_pedido_item SERIAL PRIMARY KEY,
     id_pedido INT NOT NULL,
     id_item INT NOT NULL,
     quantidade INT NOT NULL DEFAULT 1 CHECK (quantidade > 0),
