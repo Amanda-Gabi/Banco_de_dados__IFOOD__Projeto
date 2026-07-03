5f916a9e2f388037ce8d0f17e5bdaf5ee2c92924
Amanda-Gabi <gabriellaamanda114@gmail.com>
2026-07-03 13:37:58 -0300

    Update 001_inicial_IFOOD.sql
    atualização da lógica do projeto

--- migrations/001_inicial_IFOOD.sql
+++ migrations/001_inicial_IFOOD.sql
@@ -1,28 +1,26 @@
 CREATE TABLE usuario (
-    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     senha_hash VARCHAR(255) NOT NULL,
     telefone VARCHAR(15),
-    endereco_padrao TEXT NOT NULL,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+
 CREATE TABLE estabelecimento (
-    id_estabelecimento INT PRIMARY KEY AUTO_INCREMENT,
+    id_estabelecimento SERIAL PRIMARY KEY,
     nome_fantasia VARCHAR(100) NOT NULL,
     cnpj VARCHAR(18) UNIQUE NOT NULL,
     tipo_cozinha VARCHAR(50),
-    endereco TEXT NOT NULL,
-    horario_abertura TIME,
-    horario_fechamento TIME,
-    taxa_frete_fixo DECIMAL(5,2) DEFAULT 0.00,
-    status ENUM('aberto','fechado','pausado') DEFAULT 'fechado',
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    taxa_frete_fixo DECIMAL(8,2) DEFAULT 0.00,
+    status VARCHAR(20) CHECK (status IN ('aberto','fechado','pausado')) DEFAULT 'fechado',
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+
 CREATE TABLE item_cardapio (
-    id_item INT PRIMARY KEY AUTO_INCREMENT,
+    id_item SERIAL PRIMARY KEY,
     id_estabelecimento INT NOT NULL,
     nome VARCHAR(100) NOT NULL,
     descricao TEXT,
@@ -31,5 +29,34 @@ CREATE TABLE item_cardapio (
     disponivel BOOLEAN DEFAULT TRUE,
     FOREIGN KEY (id_estabelecimento) 
         REFERENCES estabelecimento(id_estabelecimento)
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE entregador (
+    id_entregador SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    telefone VARCHAR(15),
+    veiculo VARCHAR(50),
+    placa VARCHAR(10),
+    status VARCHAR(20) CHECK (status IN ('disponivel','ocupado','offline')) DEFAULT 'offline',
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE endereco (
+    id_endereco SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    apelido VARCHAR(50),
+    cep VARCHAR(9) NOT NULL,
+    logradouro VARCHAR(150) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    complemento VARCHAR(100),
+    bairro VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    estado CHAR(2) NOT NULL,
+    FOREIGN KEY (id_usuario) 
+        REFERENCES usuario(id_usuario)
         ON DELETE CASCADE
 );
