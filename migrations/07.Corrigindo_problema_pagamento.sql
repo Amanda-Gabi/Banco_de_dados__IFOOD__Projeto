f06ab760c6990afd2109320bead9fc8c8c80b0d5
Amanda-Gabi <gabriellaamanda114@gmail.com>
2026-07-06 12:50:46 -0300

    Update 07.Corrigindo_problema_pagamento.sql
    Adicionei o UNIQUE pois tinha esquecido, ele é importante pois mostra que cada entrega tem no máximo um pagamento

--- migrations/07.Corrigindo_problema_pagamento.sql
+++ migrations/07.Corrigindo_problema_pagamento.sql
@@ -1,4 +1,4 @@
 ALTER TABLE pagamento 
-    ADD COLUMN id_entrega INT,
+    ADD COLUMN id_entrega INT UNIQUE,
     ADD CONSTRAINT fk_pagamento_entrega 
     FOREIGN KEY (id_entrega) REFERENCES entrega(id_entrega);
