fdb380f016cc5ec32da64bfdb551d1aef39f441d
Amanda-Gabi <gabriellaamanda114@gmail.com>
2026-07-06 12:32:44 -0300

    Update database.sql
    adicionei a migration 06

--- database.sql
+++ database.sql
@@ -60,6 +60,7 @@ CREATE TABLE endereco (
         ON DELETE CASCADE
 );
 
+
 CREATE TABLE pedido (
     id_pedido SERIAL PRIMARY KEY,
     id_usuario INT NOT NULL,
@@ -74,6 +75,7 @@ CREATE TABLE pedido (
     FOREIGN KEY (id_endereco) REFERENCES endereco(id_endereco)
 );
 
+
 CREATE TABLE pedido_item (
     id_pedido_item SERIAL PRIMARY KEY,
     id_pedido INT NOT NULL,
@@ -85,6 +87,7 @@ CREATE TABLE pedido_item (
     FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido) ON DELETE CASCADE,
     FOREIGN KEY (id_item) REFERENCES item_cardapio(id_item)
 );
+
 
 ALTER TABLE endereco 
     ADD COLUMN latitude DECIMAL(10,8),
@@ -106,6 +109,7 @@ CREATE TABLE horario_especial (
         REFERENCES estabelecimento(id_estabelecimento)
 );
 
+
 CREATE TABLE avaliacao (
     id_avaliacao SERIAL PRIMARY KEY,
     id_pedido INT UNIQUE NOT NULL,
@@ -119,6 +123,7 @@ CREATE TABLE avaliacao (
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
     FOREIGN KEY (id_estabelecimento) REFERENCES estabelecimento(id_estabelecimento)
 );
+
 
 ALTER TABLE usuario 
     ADD CONSTRAINT check_telefone_usuario 
@@ -150,4 +155,34 @@ CREATE TABLE entrega (
     data_fim TIMESTAMP,
     FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
     FOREIGN KEY (id_entregador) REFERENCES entregador(id_entregador)
+);
+
+
+CREATE TABLE pagamento (
+    id_pagamento SERIAL PRIMARY KEY,
+    id_pedido INT UNIQUE NOT NULL,
+    metodo VARCHAR(30) CHECK (metodo IN ('cartao_credito','cartao_debito','pix','dinheiro','vale_refeicao')),
+    valor DECIMAL(10,2) NOT NULL,
+    status VARCHAR(20) CHECK (status IN ('pendente','aprovado','recusado','estornado')),
+    data_processamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido)
+);
+
+CREATE TABLE cupom (
+    id_cupom SERIAL PRIMARY KEY,
+    codigo VARCHAR(20) UNIQUE NOT NULL,
+    desconto DECIMAL(5,2) NOT NULL,
+    tipo_desconto VARCHAR(20) CHECK (tipo_desconto IN ('porcentagem','valor_fixo')),
+    valor_minimo DECIMAL(10,2) DEFAULT 0.00,
+    validade DATE,
+    ativo BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE pedido_cupom (
+    id_pedido INT NOT NULL,
+    id_cupom INT NOT NULL,
+    PRIMARY KEY (id_pedido, id_cupom),
+    FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
+    FOREIGN KEY (id_cupom) REFERENCES cupom(id_cupom)
 );
